53237ce9a17cd73abce407820b70c185f020d751
Attila Keszthelyi <syke80@gmail.com>
2017-03-28 14:39:10 +0200

    Syntax fixes

--- app/ConfigService.js
+++ app/ConfigService.js
@@ -1,12 +1,4 @@
 function ConfigService() {
-/*
-    this.save = function(id, properties) {
-        var serializedConfig = localStorage.getItem("config") || "{}",
-            config =  JSON.parse(serializedConfig);
-        config[id] = properties;
-        localStorage.setItem("config", JSON.stringify(config));
-    }
-*/
     this.save = function(properties) {
         localStorage.setItem("config", JSON.stringify(properties));
     }
